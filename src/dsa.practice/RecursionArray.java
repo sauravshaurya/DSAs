798291b7f820af15cf83dacfdc13741f7e159cf1
Saurav Shaurya <saurav.shaurya@non.se.com>
2023-12-19 22:29:22 +0530

    Added multi linear search

--- src/dsa.practice/RecursionArray.java
+++ src/dsa.practice/RecursionArray.java
@@ -1,5 +1,8 @@
 package dsa.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecursionArray {
 
     public static boolean isSortedArray(int[] arr) {
@@ -20,18 +23,36 @@ public class RecursionArray {
     }
 
     public static int helperLinearSearch(int[] arr, int target, int index) {
-        if(index == arr.length-1) {
+        if (index == arr.length - 1) {
             return -1;
         }
-        if(arr[index] == target) {
+        if (arr[index] == target) {
             return index;
         }
-        return helperLinearSearch(arr, target, index+1);
+        return helperLinearSearch(arr, target, index + 1);
     }
 
+    public static List<Integer> linearMultiSearch(int[] arr, int target) {
+        List<Integer> result = new ArrayList<>();
+        return helperMultiLinearSearch(arr, target, 0, result);
+    }
+
+    public static List<Integer> helperMultiLinearSearch(int[] arr, int target, int index, List<Integer> result) {
+
+        if (arr[index] == target) {
+            result.add(index);
+        }
+        if (index == arr.length - 1) {
+            return result;
+        }
+        return helperMultiLinearSearch(arr, target, index + 1, result);
+    }
+
+
     public static void main(String[] args) {
-        int[] arr = {1, 4, 6, 8, 9, 2};
+        int[] arr = {1, 4, 6, 8, 9, 2, 9};
         System.out.println("isSortedArray = " + isSortedArray(arr));
         System.out.println("linearSearch = " + linearSearch(arr, 91));
+        System.out.println("linearMultiSearch = " + linearMultiSearch(arr, 9));
     }
 }
